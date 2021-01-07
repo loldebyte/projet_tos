aa19a31970803923c423c19415ee1927ece0b65e
loldebyte <qdimarellis@laposte.net>
2021-01-07 16:57:08 +0100

    wip

--- launcher.c
+++ launcher.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <stdint.h>
 #include <sys/wait.h>
+#include <assert.h>
 
 #define BUFFER 102
 
@@ -14,24 +15,23 @@ void assert_valid_input(char * input) {
     len = strlen(input);
     if(len <= 1) {
         printf("Please enter a command.\n");
-        perror("String length <= 1 : cannot be a command.\n")
+        perror("String length <= 1 : cannot be a command.\n");
         abort();
     }
     if(input[len-1] != '\n') {
         printf("Your command cant exceed 100 characters. Please try again.\n");
-        perror("Last character in command is not \\n : input exceeds buffer length of %d.\n", BUFFER)
+        perror("Last character in command is not \\n : input exceeds buffer length of %d.\n", BUFFER);
         abort();
     }
 }
 char ** split_into_arguments(char * input) {
-    char * copy = malloc(strlen(input));
+    char * copy = malloc(strlen(input)); // might be unecessary : just reorganise the str in array and dont touch anything
     strcpy(copy, input);
     char ** array;
-    char * element;
     char * token;
-    while (token = strtok(copy, ' '); token != NULL) {
+    while (token = strtok(copy, ' ') != NULL) {
         size_t len = strlen(token);
-        
+
     }
 }
 
@@ -41,4 +41,5 @@ int main(void) {
     char * buffer[BUFFER];
     fgets(buffer, BUFFER, stdin);
     assert_valid_input(input);
+    execvp(command, xargs);
 }
