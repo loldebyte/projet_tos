80024a5c155699d72eecf535b40bd839aaac39c1
loldebyte <qdimarellis@laposte.net>
2021-01-07 16:24:58 +0100

    initial commit

---
+++ launcher.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <sys/wait.h>
+
+#define BUFFER 102
+
+
+void assert_valid_input(char *);
+char ** split_into_arguments(char *);
+
+void assert_valid_input(char * input) {
+    len = strlen(input);
+    if(len <= 1) {
+        printf("Please enter a command.\n");
+        perror("String length <= 1 : cannot be a command.\n")
+        abort();
+    }
+    if(input[len-1] != '\n') {
+        printf("Your command cant exceed 100 characters. Please try again.\n");
+        perror("Last character in command is not \\n : input exceeds buffer length of %d.\n", BUFFER)
+        abort();
+    }
+}
+char ** split_into_arguments(char * input) {
+    char * copy = malloc(strlen(input));
+    strcpy(copy, input);
+    char ** array;
+    char * element;
+    char * token;
+    while (token = strtok(copy, ' '); token != NULL) {
+        size_t len = strlen(token);
+        
+    }
+}
+
+int main(void) {
+    char * command[BUFFER]
+    printf("$ ");
+    char * buffer[BUFFER];
+    fgets(buffer, BUFFER, stdin);
+    assert_valid_input(input);
+}
