77e82f9470c5df2db0136cd9e361a18d649d14c0
loldebyte <qdimarellis@laposte.net>
2021-01-07 21:41:58 +0100

    fix frees

--- launcher.c
+++ launcher.c
@@ -12,7 +12,6 @@
 
 void assert_valid_input(char *);
 size_t split_into_arguments(char *, char ***);
-void free_args(char ***, size_t);
 
 void assert_valid_input(char * input) {
     size_t len = strlen(input);
@@ -26,22 +25,11 @@ size_t split_into_arguments(char * input, char *** output) {
     while (token = strtok(curr_cell == 0 ? input : NULL, SEP), token != NULL) {
         len = strlen(token);
         *output = realloc(*output, sizeof(*output) * (curr_cell+1));
-        printf("previous len: %ld\n", curr_len);
         curr_len += len;
-        printf("new len: %ld\n", curr_len);
-        *((*output)+curr_cell) = malloc(len);
         *((*output)+curr_cell) = token;
         curr_cell++;
     }
     return curr_cell;
-}
-
-void free_args(char *** args, size_t nb_args) {
-    for (int i = nb_args-1; i>-1; i--) {
-        printf("str #%d: %s\n", i, (*args)[i]);
-        free((*args)[i]);
-    }
-    free(*args);
 }
 
 int main(void) {
@@ -52,6 +40,5 @@ int main(void) {
     char ** args = NULL;
     size_t nb_of_args = split_into_arguments(buffer, &args);
     // execvp(args[0], &args[1]);
-    printf("nb args : %ld\n", nb_of_args);
-    free_args(&args, nb_of_args);
+    free(args);
 }
