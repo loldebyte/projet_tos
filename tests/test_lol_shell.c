825f92e00747b7818b9a84fd9004043e2a7a110c
loldebyte <qdimarellis@laposte.net>
2021-01-17 16:00:17 +0100

    declare test functions & test_validate_input

---
+++ tests/test_lol_shell.c
@@ -0,0 +1,30 @@
+#include "../lol_shell.h"
+
+
+bool test_validate_input(void);
+bool test_split_into_arguments(char *, EXECUTION_CONF *);
+bool test_execute(EXECUTION_CONF *);
+bool test_set_execution_type(EXECUTION_CONF *);
+bool test_strings_are_the_same(EXECUTION_CONF *);
+bool test_exec_conf_factory(void);
+bool test_exec_conf_destructor(EXECUTION_CONF *);
+bool test_dealloc_last_argument(EXECUTION_CONF *);
+
+bool test_validate_input(void) {
+    {
+        char valid_string[BUFFER] = "my test string\n";
+        assert(assert_valid_input(&valid_string) && "String validation of valid string failed\n");
+        int len = strlen(valid_string);
+        assert(valid_string[len-1] == 0 && "trailing \\n not removed !\n");
+    }
+    {
+        char too_short_string[BUFFER] = "a\n";
+        assert(assert_valid_input(&too_short_string) == false
+               && "Invalid string didnt fail !");
+    }
+    {
+        char too_long_string[BUFFER] = "Vivamus pellentesque luctus mauris, sed convallis ligula. Nulla fermentum massa vitae scelerisque ac.";
+        assert(assert_valid_input(&too_long_string) == false
+               && "Invalid string didnt fail !");
+    }
+}
