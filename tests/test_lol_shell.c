62aef796a9d69460ae671b3866bdfb2040fdfc46
loldebyte <qdimarellis@laposte.net>
2021-01-17 16:01:47 +0100

    use new name of assert_valid_input

--- tests/test_lol_shell.c
+++ tests/test_lol_shell.c
@@ -13,18 +13,18 @@ bool test_dealloc_last_argument(EXECUTION_CONF *);
 bool test_validate_input(void) {
     {
         char valid_string[BUFFER] = "my test string\n";
-        assert(assert_valid_input(&valid_string) && "String validation of valid string failed\n");
+        assert(validate_input(&valid_string) && "String validation of valid string failed\n");
         int len = strlen(valid_string);
         assert(valid_string[len-1] == 0 && "trailing \\n not removed !\n");
     }
     {
         char too_short_string[BUFFER] = "a\n";
-        assert(assert_valid_input(&too_short_string) == false
+        assert(validate_input(&too_short_string) == false
                && "Invalid string didnt fail !");
     }
     {
         char too_long_string[BUFFER] = "Vivamus pellentesque luctus mauris, sed convallis ligula. Nulla fermentum massa vitae scelerisque ac.";
-        assert(assert_valid_input(&too_long_string) == false
+        assert(validate_input(&too_long_string) == false
                && "Invalid string didnt fail !");
     }
 }
